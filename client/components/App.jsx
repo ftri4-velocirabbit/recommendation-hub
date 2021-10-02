dfdab3204832828d18be2290e1f7a971769dc678
Parker Hutcheson <pdhutcheson@gmail.com>
2021-10-02 10:45:17 -0700

    Adding app.jsx

---
+++ client/components/App.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+import  Login  from './login.jsx';
+import  SignUp  from './signUp.jsx';
+import  Results  from './results.jsx';
+// import AddMovie from './adredMovie.jsx';
+
+const App = props => {
+  return (
+    <Router>
+      <div className="router">
+        <main>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={Login}
+              />
+              <Route
+              exact
+              path="/signUp"
+              component={SignUp}
+              />
+              <Route
+              exact
+              path="/results"
+              component={Results}
+              />
+              <Route
+              exact
+              path="/addMovie"
+              component={AddMovie}
+              />
+          </Switch>
+        </main>
+      </div>
+    </Router>
+  );
+}
+export default App;
