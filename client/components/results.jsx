a0b1dd1c52ec11cb352397fe7d7c1a70ade049c8
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-10-04 13:50:34 -0700

    Table is working. Now to tweek with our data.

--- client/components/results.jsx
+++ client/components/results.jsx
@@ -1,34 +1,48 @@
 import React, { Component, useMemo } from 'react';
 import ReactDOM, { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
+// Table MUI stuff
+// import * as React from 'react';
+// import { DataGrid } from '@mui/x-data-grid';
+import CustomizedTables from './table.jsx';
 
 
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userGroup: '',
+      userName: ''
+    }
+
+  }
+
+
+
+
+
+
 
   render() {
     return (
-      <div className="addInfo_div">
-        <div>
+      <div>
+        <div className="addInfo_div">
           <Link to={'/addmovie'}>
             <button type="button" id="addInfo_button">Add Group</button>
           </Link>
-          <div class="divider"/>
+          <div className="divider"/>
           <Link to={'/addgroup'}>
             <button type="button" id="addInfo_button">Add Movie</button>
           </Link>
         </div>
-        <div className="results_container">
-
+        <div className="biggerDivider"/>
+        <div className="resultsTable_div">
+          <CustomizedTables/>
         </div>
       </div>
-
     );
   }
 }
-
-
-
-
 
 export default Results;
