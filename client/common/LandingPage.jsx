2620fd0e9937be53611b234c6a40f17cc804d89c
FOCUS ON SCHOOL <SCHOOL@macbook.lan>
2021-10-06 14:59:00 -0700

    Completed landing page which includes landing page feature and carousel item component

--- client/common/LandingPage.jsx
+++ client/common/LandingPage.jsx
@@ -1,40 +1,9 @@
 import React from 'react';
-
-import './LandingPage.scss';
-
-import Paper from '@mui/material/Paper';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import LandingPageFeature from '../landing_page/LandingPageFeature.jsx';
 
 export default function LandingPage() {
   return (
-    <>
-      <h1>Hi, this is Landing Page.</h1>
-      <Paper id='landing-container' elevation={3}>Inside paper.
-        <Card sx={{ maxWidth: 445 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="200"
-              image="https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F200917151634-national-cheeseburger-day-2020-stock.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Paper>
-    </>
+    <LandingPageFeature />
   );
 }
 
