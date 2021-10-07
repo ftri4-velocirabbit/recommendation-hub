a0fa817b55a8499745a8deb2494df5ba1240e8b3
jdavis <jacob.drew.davis@gmail.com>
2021-10-06 21:40:00 -0500

    settings

--- client/common/Body.jsx
+++ client/common/Body.jsx
@@ -4,14 +4,19 @@ import './Body.scss';
 
 import VerticalNavBar from './VerticalNavBar.jsx';
 import Feed from './../feed/Feed.jsx';
-
+import Settings from '../settings/Settings.jsx';
 import Stack from '@mui/material/Stack';
 
 export default function Body() {
   return (
+    // <Stack id='body' direction='row'>
+    //   <VerticalNavBar />
+    //   <Feed />
+    // </Stack>
+
     <Stack id='body' direction='row'>
       <VerticalNavBar />
-      <Feed />
+      <Settings />
     </Stack>
   );
 }
