a0bd6f94c4fe8e87a27d8034eef89928d5b9eb18
Duke Lee <leeduke90@gmail.com>
2021-10-07 12:03:29 -0400

    initial recommendations commit

--- client/my_recommendation/CategoryAccordion.jsx
+++ client/my_recommendation/CategoryAccordion.jsx
@@ -12,37 +12,32 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
 import RecommendationCard from './../common/RecommendationCard.jsx';
+import EditableRecommendation from './EditableRecommendation.jsx';
 
+export default function CategoryAccordion({ category, recommendations }) {
+	const id = category + '-accordion';
 
-export default function CategoryAccordion({
-  category,
-  recommendations,
-}) {
-
-
-  const id = category + '-accordion';
-
-  return (
-    <Accordion>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls={id}
-        id={id}
-      >
-        <Typography variant='h6' ml={1}>{category}</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Button>Add <AddIcon /></Button>
-        {recommendations &&
-          <Stack className='accordion-stack' spacing={5}>
-            {recommendations.map(rec => <RecommendationCard
-              key={rec.id}
-              recommendation={rec}
-              isEditable={true}
-            />)}
-          </Stack>
-        }
-      </AccordionDetails>
-    </Accordion>
-  );
+	return (
+		<Accordion>
+			<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={id} id={id}>
+				<Typography variant='h6' ml={1}>
+					{category}
+				</Typography>
+			</AccordionSummary>
+			<AccordionDetails>
+				<Button>
+					Add <AddIcon />
+				</Button>
+				{recommendations && (
+					<Stack className='accordion-stack' spacing={5}>
+						{recommendations.map((rec) => (
+							<RecommendationCard key={rec.id} recommendation={rec} isEditable={true} />
+						))}
+						{/* <EditableRecommendation />
+						<EditableRecommendation /> */}
+					</Stack>
+				)}
+			</AccordionDetails>
+		</Accordion>
+	);
 }
---
+++ client/my_recommendation/EditableRecommendation.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+// import './EditableRecommendation.scss';
+import RecommendationCard from './../common/RecommendationCard.jsx';
+
+export default function EditableRecommendation() {
+	return (
+		<div>
+			<RecommendationCard />
+		</div>
+	);
+}
