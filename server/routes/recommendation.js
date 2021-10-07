4031161fb8bcdf7a29ab449e90aaa8946c620802
Miguel Hernandez <miguelh72@outlook.com>
2021-10-07 19:32:39 -0400

    impleted CRUD on recommendations
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

--- server/routes/recommendation.js
+++ server/routes/recommendation.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 
-const { searchUsers } = require('../controllers/userController');
 const { findSession } = require('./../controllers/sessionController');
 const { getCookie } = require('./../controllers/cookieController');
 const {
@@ -63,17 +62,18 @@ router.patch('/:id',
 );
 
 router.delete('/:id',
-  // todo verifyUser,
+  getCookie,
+  findSession,
   deleteRecommendation,
   getUserRecommendations,
   (req, res, next) => {
-    if (!res.locals.user) return res.status(401).json({
+    if (!res.locals.session) return res.status(401).json({
       error: 'User is not authorized.'
     });
     if (!res.locals.dbStatus) return res.status(400).json({
       error: 'Failed to delete recommendation.'
     });
-    if (!res.local.recommendations) return next(new Error('Route reached with authorized user and but res.local.recommendations was not set.'));
+    if (!res.locals.recommendations) return next(new Error('Route reached with authorized user and but res.local.recommendations was not set.'));
 
     return res.json({ recommendations: res.locals.recommendations });
   }
